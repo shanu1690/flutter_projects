d3bc6aeeed2359d0d92c04bb5ae2e58671aefa5c
shanu1690 <shanu1690@users.noreply.github.com>
2024-07-15 11:26:03 +0000

    deploy: a1f75c88d63506d615aa2b24979d6f2e8c624594

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,7 +19,7 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb0489
 "canvaskit/skwasm.js": "5d4f9263ec93efeb022bb14a3881d240",
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "4b559fec016d145fe94bfb4f9045e369",
+"flutter_bootstrap.js": "2785784c70246965e09b700c42c5a4ea",
 "version.json": "24c0c8bb3fdf081ff5dfa4ede3c821a1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/NOTICES": "6aa16ec7a77cc57a43c893e24e5cea84",
