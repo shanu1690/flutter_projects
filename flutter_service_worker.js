b4a0dc0dca43a9f9ebda51be86cb4252b69ebef8
shanu1690 <shanu1690@users.noreply.github.com>
2024-07-17 08:39:55 +0000

    deploy: a7345f8f429f51c06c4124bc0594264006ba0bad

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "ac5d33c4edc48e7b7fc1fa9be1a60c57",
+const RESOURCES = {"flutter_bootstrap.js": "f8e17f05e3e06048379bbc76cf3e6c6f",
 "version.json": "24c0c8bb3fdf081ff5dfa4ede3c821a1",
 "index.html": "62ec173cb84c4be58b112753835f327b",
 "/": "62ec173cb84c4be58b112753835f327b",
